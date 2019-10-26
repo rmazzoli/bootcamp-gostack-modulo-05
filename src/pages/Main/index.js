d24b1cdc83b1b2d4c0895934f726e5f7b252cedc
rmazzoli <rafael.mazzoli@hotmail.com>
2019-10-25 22:32:44 -0300

    Navegação de rotas

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
@@ -82,10 +83,12 @@ export default class Main extends Component {
         </Form>
 
         <List>
-          {repositories.map(repostory => (
-            <li key={repostory.name}>
-              <span>{repostory.name}</span>
-              <a href="">Detalhes</a>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
